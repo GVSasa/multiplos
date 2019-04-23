2b99116d2417b731a77e47e918cbb9cbfe9970d5
Sebastian Salazar <sebastiancitodelasierra@hotmail.com>
2019-04-22 19:41:24 -0600

    Tarea

---
+++ Multiplos/Multiplos/Classes/LeerNum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Multiplos.Classes
+{
+    class LeerNum
+    {
+        string lineas = File.ReadAllText(@"C:\Users\sebas\Source\Repos\multiplos\Multiplos\Multiplos\Num.txt");
+
+    
+
+        //Leer todo el texto en ves de columnas 
+        public string[] divlin()
+        {
+
+            string[] arreglo = lineas.Split(new char[]{'\t', '\n'});
+            //partir el arreglo con split
+
+            
+
+            return arreglo;
+
+
+        }
+
+    }
+}
---
+++ Multiplos/Multiplos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Multiplos.Classes;
+using System.IO;
+
+namespace Multiplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LeerNum arch = new LeerNum();
+
+            //uso el metodo publico
+            
+            string[] arr2 = arch.divlin();
+
+            int a = 0;
+
+            foreach (string s in arr2)
+            {
+                //convierto strings a numeros para comparar con 0 
+                int y = int.Parse (s) % 10
+                    ;
+                if ( y == 0)
+                {
+                      a++; 
+                }
+
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
